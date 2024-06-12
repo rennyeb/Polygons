6ff5a9305ad1b523faa58ffc66d4f078eae1e35f
rennyeb <rennyeb@gmail.com>
2024-06-12 19:38:56 +0100

    More Side functionality.

--- app/src/main/java/com/bcl/polygons/Side.java
+++ app/src/main/java/com/bcl/polygons/Side.java
@@ -2,17 +2,37 @@ package com.bcl.polygons;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 public final class Side {
 
 	private final Point start;
 	private final Point end;
 	private final Point direction;
+	private final Set<Point> points = new HashSet<>();
 
 	public Side(final Point start, final Point end) {
 		this.start = start;
 		this.end = end;
 		this.direction = end.subtract(start);
+		points.add(start);
+		points.add(end);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(points);
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		try {
+			final Side side = (Side) obj;
+			return this.points.equals(side.points);
+		} catch (final ClassCastException | NullPointerException e) {
+			return false;
+		}
 	}
 
 	// TODO whether a line crosses another
@@ -47,5 +67,11 @@ public final class Side {
 	public boolean isNonAdjacentTo(final Side side) {
 		// check how many distinct vertices there are
 		return new HashSet<>(Arrays.asList(start, end, side.start, side.end)).size() == 4;
+	}
+
+	public boolean projectionContains(final Point point) {
+		return (end.getRow() - start.getRow()) * (point.getColumn() - start.getColumn()) ==
+
+				(point.getRow() - start.getRow()) * (end.getColumn() - start.getColumn());
 	}
 }
--- app/src/test/java/com/bcl/polygons/SideTest.java
+++ app/src/test/java/com/bcl/polygons/SideTest.java
@@ -7,29 +7,94 @@ import org.junit.Test;
 
 public class SideTest {
 	@Test
-	public void nonAdjacent() {
+	public void isNonAdjacentTo() {
 		final Side side1 = new Side(new Point(0, 0), new Point(0, 2));
 		// on a different row
 		final Side side2 = new Side(new Point(1, 0), new Point(1, 1));
 
 		assertThat(side1.isNonAdjacentTo(side2), equalTo(true));
+
+		// shares a vertex
+		final Side side3 = new Side(new Point(0, 2), new Point(1, 1));
+
+		assertThat(side1.isNonAdjacentTo(side3), equalTo(false));
+
+		// same side as side1
+		final Side side4 = new Side(new Point(0, 0), new Point(0, 2));
+		assertThat(side1.equals(side4), equalTo(true));
+
+		assertThat(side1.isNonAdjacentTo(side4), equalTo(false));
 	}
 
 	@Test
-	public void vertexShared() {
+	public void equals() {
 		final Side side1 = new Side(new Point(0, 0), new Point(0, 2));
-// shares a vertex
-		final Side side2 = new Side(new Point(0, 2), new Point(1, 1));
+		final Side side2 = new Side(new Point(1, 0), new Point(1, 1));
+		// reverse of side2
+		final Side side3 = new Side(new Point(1, 1), new Point(1, 0));
 
-		assertThat(side1.isNonAdjacentTo(side2), equalTo(false));
+		assertThat(side1.equals(side1), equalTo(true));
+		assertThat(side1.equals(side2), equalTo(false));
+		assertThat(side2.equals(side1), equalTo(false));
+		assertThat(side2.equals(side3), equalTo(true));
+		assertThat(side3.equals(side2), equalTo(true));
+
 	}
 
 	@Test
-	public void sameLine() {
-		final Side side1 = new Side(new Point(0, 0), new Point(0, 2));
-//same line
-		final Side side2 = new Side(new Point(0, 0), new Point(0, 2));
+	public void projectionContents() {
 
-		assertThat(side1.isNonAdjacentTo(side2), equalTo(false));
+		{
+			// horizontal
+			final Side sideHorizontal = new Side(new Point(1, 1), new Point(1, 3));
+
+			// vertices
+			assertThat(sideHorizontal.projectionContains(new Point(1, 1)), equalTo(true));
+			assertThat(sideHorizontal.projectionContains(new Point(1, 3)), equalTo(true));
+			// mid point
+			assertThat(sideHorizontal.projectionContains(new Point(1, 2)), equalTo(true));
+			// projection of line
+			assertThat(sideHorizontal.projectionContains(new Point(1, 4)), equalTo(true));
+
+			// off the line
+			assertThat(sideHorizontal.projectionContains(new Point(0, 0)), equalTo(false));
+
+		}
+
+		{
+			// vertical
+			final Side sideVertical = new Side(new Point(1, 1), new Point(3, 1));
+
+			// vertices
+			assertThat(sideVertical.projectionContains(new Point(1, 1)), equalTo(true));
+			assertThat(sideVertical.projectionContains(new Point(3, 1)), equalTo(true));
+			// mid point
+			assertThat(sideVertical.projectionContains(new Point(2, 1)), equalTo(true));
+			// projection of line
+			assertThat(sideVertical.projectionContains(new Point(4, 1)), equalTo(true));
+
+			// off the line
+			assertThat(sideVertical.projectionContains(new Point(0, 0)), equalTo(false));
+
+		}
+
+		{
+			// diagonal
+			final Side sideDiagona1 = new Side(new Point(1, 1), new Point(3, 3));
+
+			// vertices
+			assertThat(sideDiagona1.projectionContains(new Point(1, 1)), equalTo(true));
+			assertThat(sideDiagona1.projectionContains(new Point(3, 3)), equalTo(true));
+			// mid point
+			assertThat(sideDiagona1.projectionContains(new Point(2, 2)), equalTo(true));
+			// projection of line
+			assertThat(sideDiagona1.projectionContains(new Point(4, 4)), equalTo(true));
+
+			// off the line
+			assertThat(sideDiagona1.projectionContains(new Point(1, 2)), equalTo(false));
+
+		}
+
 	}
+
 }
